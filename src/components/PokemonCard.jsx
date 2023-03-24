6b95b78e666ae9b490263d636b388becd49dd8c3
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-03-24 16:29:33 +0100

    Ajout du composent PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,10 @@
+function PokemonCard() {
+    return (
+        <figure>
+            <img src="../src/assets/bulbasaur.png"
+            alt="bulbasaur"></img>
+        </figure>
+    )
+  }
+  
+export default PokemonCard;
