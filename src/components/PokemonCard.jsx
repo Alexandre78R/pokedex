ad3b5ecae63e4d22da668bddd07377c36a0eb322
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-03-26 15:16:47 +0200

    Ajout de components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,25 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+    const pokemon = pokemonList[1];
     return (
         <figure>
-            <img src="../src/assets/bulbasaur.png"
-            alt="bulbasaur"></img>
+          {
+            pokemon.imgSrc == undefined ?
+              <p>???</p>
+              : 
+              <img src= {pokemon.imgSrc}
+              alt={pokemon.name}></img>
+              }
         </figure>
     )
   }
