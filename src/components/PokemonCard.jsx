fb42203c774d531a7542e0f2d2734236a3941e47
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-04-03 11:56:22 +0200

    Ajout correctif pokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -97,64 +97,79 @@ function PokemonCard({ pokemon, pokemonEvolutionNext, pokemonEvolutionPrev, setP
                 <h3>{pokemon.name.english} - {pokemon.name.french} - {pokemon.name.chinese} - {pokemon.name.japanese} <img style={{marginBottom : -5}} src={pokemon.image.sprite}
                 alt={pokemon.name.english}></img></h3>
               </div>
-              <img style={{backgroundColor: "White", padding:"1.5rem", borderRadius: "50%", height:"150px"}} src={pokemon.image.thumbnail}
+              <img style={{backgroundColor: "White", borderRadius: "100%", padding : "2rem", height:"150px"}} src={pokemon.image.thumbnail}
               alt={pokemon.name.english}></img>
-              <div>
-                { pokemon.base == undefined ?
-                ""
-                :
+              <div style={{display: "grid", gridTemplateColumns:"1fr 1fr", gridTemplateRows:"1fr"}}>
+                <div style={{gridArea: "1 / 1 / 2 / 2"}}>
                 <div>
-                  <p style={{fontSize:"150%",margin:0, display:"flex", padding: "2rem", marginLeft : -15}}>Bases stats :</p>
-                  <p style={{fontSize:"120%",margin:0, display:"flex", marginTop:-20, marginLeft : 15}}>Attack : {pokemon.base.Attack} - Defense : {pokemon.base.Defense} - HP : {pokemon.base.HP} - Speed : {pokemon.base.Speed}</p>
-                </div>
-                }
-              </div>
-              <div>
-                { pokemon.profile == undefined ?
-                ""
-                :
-                <div>
-                  <p style={{fontSize:"150%",margin:0, display:"flex", padding: "2rem", marginLeft : -15}}>Information Profile :</p>
-                  <p style={{fontSize:"120%",margin:0, display:"flex", marginTop:-20, marginLeft : 15}}>Heigth : {pokemon.profile.height} - Weight : {pokemon.profile.weight}</p>
-                </div>
-                }
-              </div>
-              <div>
-                { pokemonEvolutionNext.length === 0 && pokemonEvolutionPrev.length === 0 ? 
+                  { pokemon.base == undefined ?
                   ""
                   :
-                  <p style={{fontSize:"150%",margin:0, display:"flex", padding: "2rem", marginLeft : -15}}>Evolution :</p>
-                }
-                { pokemonEvolutionPrev.length === 0 ? 
-                ""
-                :
-                <p style={{fontSize:"120%",margin:0, display:"flex", marginTop:-20, marginLeft : 15}}>Evolution Previou :</p>
-                }
-                { pokemonEvolutionPrev.length === 0 ?
-                ""
-                :  
-                pokemonEvolutionPrev.map((pokemon, i) => 
-                  <div onClick={() => switchPokemon(false, i)}>
-                    <img style={{marginBottom : -5, margin:0, display:"flex", marginLeft : 15}} src={pokemon.image.sprite}
-                    alt={pokemon.name.english}></img> 
-                    <p style={{mdargin:0, display:"flex", marginLeft : 15, marginBottom : 10}}>{pokemon.name.english}</p>
+                  <div>
+                    <p style={{fontSize:"150%",margin:0, display:"flex", padding: "2rem", marginLeft : -15, marginBottom: -30, marginTop: -1}}>Bases stats :</p>
+                    <ul style={{margin:0,marginLeft : -150}}>
+                      <p style={{fontSize:"120%"}}>Attack : {pokemon.base.Attack}</p>
+                      <p style={{fontSize:"120%"}}>Defense : {pokemon.base.Defense}</p>
+                      <p style={{fontSize:"120%"}}>HP : {pokemon.base.HP}</p>
+                      <p style={{fontSize:"120%"}}>Speed : {pokemon.base.Speed}</p>
+                    </ul>
                   </div>
-                )}
-                { pokemonEvolutionNext.length === 0 ? 
-                ""
-                :
-                <p style={{fontSize:"120%",margin:0, display:"flex", marginTop:-20, marginLeft : 15}}>Evolution Next:</p>
-                }
-                { pokemonEvolutionNext.length === 0 ?
-                ""
-                :
-                  pokemonEvolutionNext.map((pokemon, i) =>
-                    <div onClick={() => switchPokemon(true, i)} >
-                      <img style={{marginBottom : -5, margin:0, display:"flex", marginLeft : 15}} src={pokemon.image.sprite}
-                      alt={pokemon.name.english}></img>
-                      <p style={{margin:0, display:"flex", marginLeft : 15, marginBottom : 10}}>{pokemon.name.english}</p>
-                    </div>
-                )}
+                  }
+                </div>
+                <div>
+                  { pokemon.profile == undefined ?
+                  ""
+                  :
+                  <div>
+                    <p style={{fontSize:"150%",margin:0, display:"flex", padding: "2rem", marginLeft : -15}}>Information Profile :</p>
+                    <p style={{fontSize:"120%",margin:0, display:"flex", marginTop:-20, marginLeft : 15}}>Heigth : {pokemon.profile.height} - Weight : {pokemon.profile.weight}</p>
+                  </div>
+                  }
+                </div>
+                </div>
+                <div style={{gridArea: "1 / 2 / 2 / 3"}}>
+                  { pokemonEvolutionNext.length === 0 && pokemonEvolutionPrev.length === 0 ? 
+                    ""
+                    :
+                    <p style={{fontSize:"150%",margin:0, display:"flex", padding: "2rem", marginLeft : -15}}>Evolution :</p>
+                  }
+                <div>
+                  { pokemonEvolutionPrev.length === 0 ? 
+                  ""
+                  :
+                  <p style={{fontSize:"120%",margin:0, marginRight: 100}}>Evolution Previou :</p>
+                  }
+                  <div style={{display: "flex", flexWrap: "wrap"}}>
+                    { pokemonEvolutionPrev.length === 0 ?
+                    ""
+                    : 
+                    pokemonEvolutionPrev.map((pokemon, i) => 
+                      <div onClick={() => switchPokemon(false, i)}>
+                        <img style={{marginBottom : -5, margin:0}} src={pokemon.image.sprite}
+                        alt={pokemon.name.english}></img> 
+                        <p style={{mdargin:0, display:"flex", marginLeft : 15, marginBottom : 10}}>{pokemon.name.english}</p>
+                      </div>
+                    )}
+                  </div>
+                  { pokemonEvolutionNext.length === 0 ? 
+                  ""
+                  :
+                  <p style={{fontSize:"120%",margin:0, marginRight: 120}}>Evolution Next:</p>
+                  }
+                  <div style={{display: "flex", flexWrap: "wrap"}}>
+                    { pokemonEvolutionNext.length === 0 ?
+                    ""
+                    :
+                      pokemonEvolutionNext.map((pokemon, i) =>
+                        <div onClick={() => switchPokemon(true, i)} >
+                          <img style={{marginBottom : -5, margin:0, display:"flex", marginLeft : 15}} src={pokemon.image.sprite}
+                          alt={pokemon.name.english}></img>
+                          <p style={{margin:0, display:"flex", marginLeft : 15, marginBottom : 10}}>{pokemon.name.english}</p>
+                        </div>
+                    )}
+                  </div>
+                </div>
+                </div>
               </div>
               <p style={{display: "flex", fontSize:"140%", backgroundColor:"white", borderRadius: 10, color: colors[pokemon.type[0]], fontWeight:"bold"}}>{pokemon.description}</p>
             </div>
